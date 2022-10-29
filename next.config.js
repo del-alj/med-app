ec0ae7af85e4484656587464888d7fa721e14b90
del-alj <woo.dong.7.wd@gmail.com>
2022-10-29 23:53:33 +0100

    backup

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [{
+      source: '/login',
+      destination: '/',
+      permanent: true,
+    },]
+  },
 }
 
 module.exports = nextConfig
--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -1,16 +1,22 @@
 
 import { getUserRoute } from "../../modules/user.js"
-export const login = async (values) => {
+import { sign } from 'jsonwebtoken';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export const login = async (req, res) => {
+    
+    console.log("-------------", req)
     const name = values?.username;
     return await getUserRoute(name).then((res) => {
-        if (res?.password === values?.password && res?.isActive)
-            return res;
-        else if (res?.password === values?.password && !res?.isActive)
-            throw new Error('Unauthorized access : user Bloked');
+        if (res?.password === values?.password && res?.isActive) {
+            const jwt = sign(res, "0e900be1-0ac5-4e6a-bf4b-38f8b21a189b", { expiresIn: '1h' });
+            res.json({ authToken: jwt });
+        } else if (res?.password === values?.password && !res?.isActive)
+            res.json({ message: 'Unauthorized access : user Bloked' });
         else
-            throw new Error('Unauthorized access: bad password');;
+            res.json({ message: 'Unauthorized access: bad password' });
     }).catch((err) => {
         console.log(err)
-        return 'Unauthorized access: user does not exist';
+        res.json({ message: 'Unauthorized access: user does not exist' });
     });
 };
